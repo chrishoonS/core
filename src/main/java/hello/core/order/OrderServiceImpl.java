e7c2ba60e274f1cb3ba7f04285e106505b74f755
송지훈 <jihoon.song@medisolution.co.kr>
2024-01-17 09:43:48 +0900

    - 스프링 핵심원리 이해2:객체지향 원리 적용 - OCP, DIP 위반 여부 확인

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,29 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    /*
+     * 할인 정책 변경을 위해 interface 교체
+     * OCP, DIP 같은 객체 지향 설계 원칙을 충실히 준수 했나?
+     * 
+     * DIP : 클래스 의존관계를 보면
+     *  - 추상(interface) 뿐만 아니라 구체(impl) 클래스에도 의존하고 있다.
+     *  - 추상(interface) 의존: `DiscountPolicy`
+     *  - 구체(impl) 클래스: `FixDiscountPolicy` , `RateDiscountPolicy`
+     *
+     * 지금 코드는 기능을 확장해서 변경하면, 클라이언트 코드에 영향을 준다! 따라서 OCP를 위반
+     *
+     * 이 문제를 해결하려면?
+     * 클라이언트 코드인 `OrderServiceImpl` 은 `DiscountPolicy` 의 인터페이스 뿐만 아니라 구체 클래스도 함 께 의존한다.
+     * 그래서 구체 클래스를 변경할 때 클라이언트 코드도 함께 변경해야 한다.
+     * **DIP 위반** 추상에만 의존하도록 변경(인터페이스에만 의존)
+     * DIP를 위반하지 않도록 인터페이스에만 의존하도록 의존관계를 변경하면 된다.
+     */
+
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 고정 할인
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 정액 할인
+    private DiscountPolicy discountPolicy;
+
 
     /**
      * 주문 생성 요청
