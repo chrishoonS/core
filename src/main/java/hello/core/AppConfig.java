7f43f6031e17b407e30765aa38f622545798c8b8
송지훈 <jihoon.song@medisolution.co.kr>
2024-01-18 14:06:56 +0900

    - 스프링 핵심원리 이해2:객체지향 원리 적용 - AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,16 +10,28 @@ import hello.core.order.OrderServiceImpl;
 /**
  * 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성
  * 연결하는 책임을 가지는 별도의 설정 클래스
+ *
+ * 리팩토링 후
+ * 중복도 제거하고, 메서드만 봐도 역할이 잘 드러남으로써, 애플리케이션의 전체 구성도 파악하기가 쉬어짐
  */
 public class AppConfig {
     public MemberService memberService() {
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository()); // DI(Dependency Injection, 의존관계 주입)
+        // new MemoryMemberRepository() -> cmd+option+m으로 리팩토링
+        return new MemberServiceImpl(memberRepository()); // DI(Dependency Injection, 의존관계 주입)
     }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // new FixDiscountPolicy() -> cmd+option+m으로 리팩토링
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
 
