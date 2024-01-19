1890d4cf6a3e4190135be59b850a4f4e88aa5320
송지훈 <jihoon.song@medisolution.co.kr>
2024-01-19 10:57:37 +0900

    - 스프링 핵심원리 이해2:객체지향 원리 적용 - FixDiscountPolicy -> RateDiscountPolicy로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,6 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,6 +13,8 @@ import hello.core.order.OrderServiceImpl;
  *
  * 리팩토링 후
  * 중복도 제거하고, 메서드만 봐도 역할이 잘 드러남으로써, 애플리케이션의 전체 구성도 파악하기가 쉬어짐
+ * 할인 정책 변경해도 애플리케이션 구성 역할을 담당하는 AppConfig만 수정하면 됨!(사용영역 코드는 수정 안해도됨(OrderServiceImpl 등..))
+ *
  */
 public class AppConfig {
     public MemberService memberService() {
@@ -30,8 +32,9 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private static FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    private static RateDiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
 
